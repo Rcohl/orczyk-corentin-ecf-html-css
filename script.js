2d55b5b6766e384ae347fd8164e9ac08b4b3e716
Rcohl <corentin.orczyk@outlook.fr>
2023-09-08 13:42:55 +0200

    modifications suite aux bogs

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const footer = document.querySelector("footer");
 window.addEventListener("scroll", () => {
   console.log(window.scrollY);
 
-  if (window.scrollY > 1450) {
+  if (window.scrollY > 1400) {
     footer.style.bottom = 0;
   } else {
     footer.style.bottom = "-500px";
